905a2011a8308581adb8ccc102ea2a89b1ce573d
Deives Michellis <deives.michellis@gmail.com>
2014-08-26 07:07:03 +0200

    Support for caller and PID on the logs, also accessors for the flags

--- logger.go
+++ logger.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"strings"
+	"syscall"
 )
 
 func (l *Logger) Enabled() bool {
@@ -13,11 +16,37 @@ func (l *Logger) Enabled() bool {
 	return true
 }
 
+const (
+	LShowCaller = 1 << iota
+	LShowPID
+)
+
+var LogLevel int = 99
+var logFlags = LShowPID | LShowCaller
+var NVM = Logger{prefix: "NVM  ", level: 4}
+var BTW = Logger{prefix: "BTW  ", level: 3}
+var FYI = Logger{prefix: "FYI  ", level: 2}
+var WTF = Logger{prefix: "WTF?!", level: 1}
+var FUUU = Logger{prefix: "FUUU!", level: 0}
+var FUU = Logger{prefix: "FUUU!", level: 0}
+
 func (l *Logger) Printf(format string, args ...interface{}) {
 	if !l.Enabled() {
 		return
 	}
-	log.Printf("[%s] %s", l.prefix, fmt.Sprintf(format, args...))
+	prefix := fmt.Sprintf("[%s]", l.prefix)
+
+	if logFlags&(LShowPID) != 0 {
+		prefix = fmt.Sprintf("[%5d][%s]", syscall.Getpid(), l.prefix)
+	}
+
+	caller := ""
+	if logFlags&(LShowCaller) != 0 {
+		_, file, line, _ := runtime.Caller(1)
+		split := strings.Split(file, "/")
+		caller = fmt.Sprintf(" [%s:%d]", split[len(split)-1], line)
+	}
+	log.Printf("%s %s%s", prefix, fmt.Sprintf(format, args...), caller)
 }
 
 func (l *Logger) Fatalf(format string, args ...interface{}) {
@@ -29,20 +58,20 @@ func (l *Logger) Level() int {
 	return l.level
 }
 
-var LogLevel int = 99
-var NVM = Logger{prefix: "NVM  ", level: 4}
-var BTW = Logger{prefix: "BTW  ", level: 3}
-var FYI = Logger{prefix: "FYI  ", level: 2}
-var WTF = Logger{prefix: "WTF?!", level: 1}
-var FUUU = Logger{prefix: "FUUU!", level: 0}
-var FUU = Logger{prefix: "FUUU!", level: 0}
-
 func GetLogLevel() int {
 	return LogLevel
 }
 
 func SetLogLevel(i int) {
 	LogLevel = i
+}
+
+func GetLogFlags() int {
+	return logFlags
+}
+
+func SetLogFlags(i int) {
+	logFlags = i
 }
 
 func KewlLogLevelNames() {
--- logging/importer.go
+++ logging/importer.go
@@ -4,10 +4,17 @@ import "github.com/dmichellis/gocassos"
 
 // so you can import . github.com/dmichellis/gocassos/logging and use those
 var LogLevel = &gocassos.LogLevel
+var SetLogFlags = gocassos.SetLogFlags
+var GetLogFlags = gocassos.GetLogFlags
 var SetLogLevel = gocassos.SetLogLevel
 var GetLogLevel = gocassos.GetLogLevel
 var KewlLogLevelNames = gocassos.KewlLogLevelNames
 var BoringLogLevelNames = gocassos.BoringLogLevelNames
+
+const (
+	LShowCaller = gocassos.LShowCaller
+	LShowPID    = gocassos.LShowPID
+)
 
 var FUUU = &gocassos.FUUU
 var FUU = &gocassos.FUUU
